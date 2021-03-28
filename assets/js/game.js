6b17391c6938ad56986b3d198bfa42d1fae2e306
Robert Schwartz <bschwartz23@gmail.com>
2021-03-28 13:02:57 -0700

    reset enemy health in for loop

--- assets/js/game.js
+++ assets/js/game.js
@@ -14,27 +14,31 @@ var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function(enemyName) {
+var fight = function (enemyName) {
   // Alert players that they are starting the round
   window.alert("The fight has begun!");
 
-  var promptFight = window.prompt(
-    "Would you like to FIGHT or SKIP this battle?  Enter 'FIGHT' or 'SKIP' to choose."
-  );
-  {
+  while (enemyHealth > 0) {
+    var promptFight = window.prompt(
+      "Would you like to FIGHT or SKIP this battle?  Enter 'FIGHT' or 'SKIP' to choose."
+    );
     // if player choses to fight, then fight
-    if (promptFight === "fight" || promptFight === "FIGHT") {
+    if (
+      promptFight === "fight" ||
+      promptFight === "FIGHT" ||
+      promptFight === "Fight"
+    ) {
       // remove enemy's health by subtracting the amount set in the playerAttack variable
       enemyHealth = enemyHealth - playerAttack;
       console.log(
         playerName +
-        " attacked " +
-        enemyName +
-        ". " +
-        enemyName +
-        " now has " +
-        enemyHealth +
-        " health remaining."
+          " attacked " +
+          enemyName +
+          ". " +
+          enemyName +
+          " now has " +
+          enemyHealth +
+          " health remaining."
       );
 
       // check enemy's health
@@ -48,13 +52,13 @@ var fight = function(enemyName) {
       playerHealth = playerHealth - enemyAttack;
       console.log(
         enemyName +
-        " attacked " +
-        playerName +
-        ". " +
-        playerName +
-        " now has " +
-        playerHealth +
-        " health remaining."
+          " attacked " +
+          playerName +
+          ". " +
+          playerName +
+          " now has " +
+          playerHealth +
+          " health remaining."
       );
 
       // check player's health
@@ -81,12 +85,10 @@ var fight = function(enemyName) {
       window.alert("You need to choose a valid option. Try again!");
     }
   }
-}
+};
 
 for (var i = 0; i < enemyNames.length; i++) {
-  console.log(enemyNames[i]);
-  console.log(i);
-  console.log(enemyNames[i] + " is at " + i + " index");
-  fight(enemyNames[i]);
+  var pickedEnemyName = enemyNames [i];
+  enemyHealth = 50;
+  fight(pickedEnemyName);
 }
-
